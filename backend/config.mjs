3a2c17d5bf872798c4e5308029178b517f373b09
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-22 01:06:22 +0000

    Feat:Add MongoDB and mongoose to node js

--- backend/config.mjs
+++ backend/config.mjs
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://mohcinsafi:UooiVbNUJMxT3PZP@cluster0.xdnqepl.mongodb.net/Book-Store?retryWrites=true&w=majority&appName=Cluster0";
