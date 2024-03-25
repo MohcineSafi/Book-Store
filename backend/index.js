6da55d0c804c9589efe302fcf2a108d0e484616a
MohcineSafi <safi.mouhcine02@gmail.com>
2024-03-25 01:40:52 +0000

    Feat:All Files with Fix Problems

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
-import express from "express";
-import { PORT, mongoDBURL } from "./config.mjs";
-import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
-import booksRoute from "./routes/booksRoute.js";
-import cors from "cors";
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -16,23 +15,23 @@ app.use(cors());
 // Option 2: Allow Custom Origins
 // app.use(
 //   cors({
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"],
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
 //   })
 // );
 
-app.get("/", (req, res) => {
-  console.log(req);
-  return res.status(234).send("Hello, World!");
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-app.use("/books", booksRoute);
+app.use('/books', booksRoute);
 
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log("App connected to database");
+    console.log('App connected to database');
     app.listen(PORT, () => {
       console.log(`App is listening to port: ${PORT}`);
     });
